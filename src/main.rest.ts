ccb7819d275071fc87e892150a2ec5c6b492fe62
TyulenOl <setu787898@gmail.com>
2024-04-15 23:28:46 +0500

    module5-task2

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,12 +4,14 @@ import { createRestApplicationContainer, RestApplication } from './rest/index.js
 import { Component } from './shared/types/index.js';
 import { createUserContainer } from './shared/modules/user/index.js';
 import { createOfferContainer } from './shared/modules/offer/index.js';
+import { createCommentContainer } from './shared/modules/comment/index.js';
 
 async function bootstrap() {
   const appContainer = Container.merge(
     createRestApplicationContainer(),
     createUserContainer(),
-    createOfferContainer()
+    createOfferContainer(),
+    createCommentContainer()
   );
 
   const application = appContainer.get<RestApplication>(Component.RestApplication);
---
+++ src/shared/modules/comment/comment.entity.ts
@@ -0,0 +1,37 @@
+import { defaultClasses, getModelForClass, modelOptions, prop, Ref } from '@typegoose/typegoose';
+import { UserEntity } from '../user/index.js';
+import { OfferEntity } from '../offer/index.js';
+
+export interface CommentEntity extends defaultClasses.Base {
+}
+
+@modelOptions({
+  schemaOptions: {
+    collection: 'comments'
+  }
+})
+
+
+export class CommentEntity extends defaultClasses.TimeStamps {
+  @prop({
+    ref: OfferEntity,
+    required: true,
+    type: String
+  })
+  public offerId: Ref<OfferEntity>;
+
+  @prop({type: String, minlength: 5, maxlength: 1024, trim: true, required: true})
+  public text: string;
+
+  @prop({type: Number, min: 1, max: 5, required: true})
+  public rating: number;
+
+  @prop({
+    ref: UserEntity,
+    required: true,
+    type: String
+  })
+  public userId: Ref<UserEntity>;
+}
+
+export const CommentModel = getModelForClass(CommentEntity);
